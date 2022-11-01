a2d2c4a132ce7c660babe330eed46d1162e51cc4
AFejes22 <afejes22@gmail.com>
2022-11-01 21:43:51 +0100

    collision method altered

--- src/entity/NPC_OldMan.java
+++ src/entity/NPC_OldMan.java
@@ -32,21 +32,30 @@ public class NPC_OldMan extends Entity{
     }
 
     public void setAction(){ // kind of AI :)
-        Random random = new Random();
-        int i = random.nextInt(100)+1;
 
-        if(i <= 25){
-            direction = "up";
+        actionLockCounter++;
+
+        if(actionLockCounter == 120){
+            Random random = new Random();
+            int i = random.nextInt(100)+1;
+
+            if(i <= 25){
+                direction = "up";
+            }
+            if(i > 25 && i <= 50){
+                direction = "down";
+            }
+            if(i > 50 && i <= 75){
+                direction = "left";
+            }
+            if(i > 75 && i <= 100){
+                direction = "right";
+            }
+
+            actionLockCounter = 0;
         }
-        if(i > 25 && i <= 50){
-            direction = "down";
-        }
-        if(i > 50 && i <= 75){
-            direction = "left";
-        }
-        if(i > 75 && i <= 100){
-            direction = "right";
-        }
+
+
     }
 
 
