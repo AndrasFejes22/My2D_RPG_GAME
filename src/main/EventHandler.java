f3d9e0c811f801c6c9526793169933e1e9e20e9d
AFejes22 <afejes22@gmail.com>
2022-11-03 23:40:23 +0100

    EventHandler events changed

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -26,6 +26,12 @@ public class EventHandler {
             eventRect[col][row].height = 2;
             eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
             eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
+
+            col++;
+            if(col == gp.maxWorldCol){
+                col = 0;
+                row++;
+            }
         }
 
 
@@ -35,37 +41,37 @@ public class EventHandler {
 
         if(hit(27,17, "right") == true){
             System.out.println("Pit damage!");
-            damagePit(gp.dialogueState);
+            damagePit(27, 17, gp.dialogueState);
         }
 
         if(hit(23,12, "up") == true){
             System.out.println("Healing!");
-            healingPool(gp.dialogueState);
+            healingPool(23, 12, gp.dialogueState);
         }
 
         if(hit(26,20, "right") == true){
             System.out.println("Teleport!");
-            teleport(gp.dialogueState);
+            teleport(26, 20, gp.dialogueState);
         }
-
-
 
     }
 
-    private void teleport(int gameState) {
+    // EVENTS
+
+    private void teleport(int col, int row, int gameState) {
         gp.gameState = gameState;
         gp.ui.currentDialogue = "Teleport!";
         gp.player.worldX = gp.tileSize * 37;
         gp.player.worldY = gp.tileSize * 10;
     }
 
-    private void damagePit(int gameState) {
+    private void damagePit(int col, int row, int gameState) {
         gp.gameState = gameState;
         gp.ui.currentDialogue = "You fall into a pit!";
         gp.player.life -= 1;
     }
 
-    private void healingPool(int gameState) {
+    private void healingPool(int col, int row, int gameState) {
         if(gp.keyH.enterPressed == true){
             gp.gameState = gameState;
             gp.ui.currentDialogue = "You have drank water. \nYour life has been recovered!";
@@ -74,16 +80,18 @@ public class EventHandler {
 
     }
 
-    public boolean hit (int eventCol, int eventRow, String reqDirection){
+    // check boolean
+
+    public boolean hit (int col, int row, String reqDirection){
 
         boolean hit = false;
 
         gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
         gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
-        eventRect.x = eventCol * gp.tileSize + eventRect.x;
-        eventRect.y = eventRow * gp.tileSize + eventRect.y;
+        eventRect[col][row].x = col * gp.tileSize + eventRect[col][row].x;
+        eventRect[col][row].y = row * gp.tileSize + eventRect[col][row].y;
 
-        if(gp.player.solidArea.intersects(eventRect)){
+        if(gp.player.solidArea.intersects(eventRect[col][row])){
             if(gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")){
                 hit = true;
             }
@@ -92,8 +100,8 @@ public class EventHandler {
         //reset:
         gp.player.solidArea.x = gp.player.solidAreaDefaultX;
         gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-        eventRect.x = eventRectDefaultX;
-        eventRect.y = eventRectDefaultY;
+        eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
+        eventRect[col][row].y = eventRect[col][row].eventRectDefaultY;
 
 
         return hit;
