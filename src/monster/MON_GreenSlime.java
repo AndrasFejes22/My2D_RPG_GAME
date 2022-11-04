b9ba5aa5e114a787ace4361820972f5efc9d7048
AFejes22 <afejes22@gmail.com>
2022-11-04 19:56:46 +0100

    Monster package/MON_GreenSlime class created

---
+++ src/monster/MON_GreenSlime.java
@@ -0,0 +1,28 @@
+package monster;
+
+import entity.Entity;
+import main.GamePanel;
+
+public class MON_GreenSlime extends Entity {
+
+    public MON_GreenSlime(GamePanel gp) {
+        super(gp);
+
+        //sets:
+        name = "Green Slime";
+        speed = 1;
+        maxLife = 4;
+        life = maxLife;
+
+        //solidArea:
+
+        solidArea.x = 3;
+        solidArea.y = 18;
+        solidArea.width = 42;
+        solidArea.height = 30;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
+
+
+    }
+}
