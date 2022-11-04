fdac0b5b4b6c95a4273a572b010b6ee330de1f2b
AFejes22 <afejes22@gmail.com>
2022-11-04 20:17:25 +0100

    MON_GreenSlime class altered

--- src/monster/MON_GreenSlime.java
+++ src/monster/MON_GreenSlime.java
@@ -3,6 +3,8 @@ package monster;
 import entity.Entity;
 import main.GamePanel;
 
+import java.util.Random;
+
 public class MON_GreenSlime extends Entity {
 
     public MON_GreenSlime(GamePanel gp) {
@@ -22,6 +24,44 @@ public class MON_GreenSlime extends Entity {
         solidArea.height = 30;
         solidAreaDefaultX = solidArea.x;
         solidAreaDefaultY = solidArea.y;
+        getImage();
+
+    }
+
+    public void getImage(){
+        up1 = setup("/monster/greenslime_down1");
+        up2 = setup("/monster/greenslime_down2");
+        down1 = setup("/monster/greenslime_down1");
+        down2 = setup("/monster/greenslime_down2");
+        left1 = setup("/monster/greenslime_down1");
+        left2 = setup("/monster/greenslime_down2");
+        right1 = setup("/monster/greenslime_down1");
+        right2 = setup("/monster/greenslime_down2");
+    }
+
+    public void setAction(){ // kind of AI :)
+
+        actionLockCounter++;
+
+        if(actionLockCounter == 120){
+            Random random = new Random();
+            int i = random.nextInt(100)+1;
+
+            if(i <= 25){
+                direction = "up";
+            }
+            if(i > 25 && i <= 50){
+                direction = "down";
+            }
+            if(i > 50 && i <= 75){
+                direction = "left";
+            }
+            if(i > 75 && i <= 100){
+                direction = "right";
+            }
+
+            actionLockCounter = 0;
+        }
 
 
     }
