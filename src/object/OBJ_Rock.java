f7ab6d36960484e1d77af55d2c20f28356901157
AFejes22 <afejes22@gmail.com>
2022-11-09 23:12:24 +0100

    OBJ_Rock class created

---
+++ src/object/OBJ_Rock.java
@@ -0,0 +1,42 @@
+package object;
+
+import entity.Projectile;
+import main.GamePanel;
+
+public class OBJ_Rock extends Projectile {
+
+    //GamePanel gp;
+
+    public OBJ_Rock(GamePanel gp) {
+
+        super(gp);
+
+        name = "Rock";
+        speed = 8; // RPG-szerűen változtatható, pl ha valamely tárgy vagy képesség boostolja
+        maxLife = 80; // RPG-szerűen változtatható, pl ha valamely tárgy vagy képesség boostolja, hogy mennyi idő után tűnik el
+        life = maxLife;
+        attack = 2;
+        useCost = 1; //use 1 mana to cast spell
+        alive = false;
+        getImage();
+
+
+        //attackArea.width = 30;
+        //attackArea.height = 30;
+
+        //description = "[" + name + "] \nA bit rusty but still can cut\n some trees.";
+
+
+    }
+
+    private void getImage() {
+        up1 = setup("/projectile/rock_down_1", gp.tileSize, gp.tileSize);
+        up2 = setup("/projectile/rock_down_1", gp.tileSize, gp.tileSize);
+        down1 = setup("/projectile/rock_down_1", gp.tileSize, gp.tileSize);
+        down2 = setup("/projectile/rock_down_1", gp.tileSize, gp.tileSize);
+        left1 = setup("/projectile/rock_down_1", gp.tileSize, gp.tileSize);
+        left2 = setup("/projectile/rock_down_1", gp.tileSize, gp.tileSize);
+        right1 = setup("/projectile/rock_down_1", gp.tileSize, gp.tileSize);
+        right2 = setup("/projectile/rock_down_1", gp.tileSize, gp.tileSize);
+    }
+}
