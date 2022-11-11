81fc50437b406f1cb6810bb15a9d7893056058ec
AFejes22 <afejes22@gmail.com>
2022-11-11 20:39:23 +0100

    interactive tiles: object, packages, class added

---
+++ src/interactiveTiles/InteractiveTile.java
@@ -0,0 +1,11 @@
+package interactiveTiles;
+
+import entity.Entity;
+import main.GamePanel;
+
+public class InteractiveTile extends Entity {
+
+    public InteractiveTile(GamePanel gp) {
+        super(gp);
+    }
+}
