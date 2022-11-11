5cc01858ef535345a17d5deaae1cca8be3d4ed16
AFejes22 <afejes22@gmail.com>
2022-11-12 00:52:51 +0100

    Particle updated

--- src/entity/Particle.java
+++ src/entity/Particle.java
@@ -27,4 +27,16 @@ public class Particle extends Entity{
         worldX = generator.worldX;
         worldY = generator.worldY;
     }
+
+    public void update(){
+
+    }
+
+    public void draw(Graphics2D g2){
+        int screenX = worldX - gp.player.worldX + gp.player.screenX;
+        int screenY = worldY - gp.player.worldY + gp.player.screenY;
+
+        g2.setColor(color);
+        g2.fillRect(screenX, screenY, size, size);
+    }
 }
