97ffb770c371347d71b153163a22f95d3d3a2f8e
AFejes22 <afejes22@gmail.com>
2022-11-12 12:38:45 +0100

    draw() method override in InteractiveTile class

--- src/interactiveTiles/InteractiveTile.java
+++ src/interactiveTiles/InteractiveTile.java
@@ -3,6 +3,8 @@ package interactiveTiles;
 import entity.Entity;
 import main.GamePanel;
 
+import java.awt.*;
+
 public class InteractiveTile extends Entity {
 
     public boolean desctructible = false;
@@ -33,5 +35,20 @@ public class InteractiveTile extends Entity {
     public boolean isCorrectItem (Entity entity){
         boolean isCorrectItem = false;
         return isCorrectItem;
+    }
+
+    public void draw(Graphics2D g2) {
+
+        int screenX = worldX - gp.player.worldX + gp.player.screenX;
+        int screenY = worldY - gp.player.worldY + gp.player.screenY;
+
+        //if statement : only drawing the visible tiles, not the whole 50x50 world
+        if(worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
+                worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+                worldY + gp.tileSize  > gp.player.worldY - gp.player.screenY &&
+                worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
+
+            g2.drawImage(down1, screenX, screenY, null);
+        }
     }
 }
