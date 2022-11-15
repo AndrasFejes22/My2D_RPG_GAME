eac88cbe42f2d8bf118dc4af6b84a247b1b9ba48
AFejes22 <afejes22@gmail.com>
2022-11-15 21:09:15 +0100

    NPC_Merchant set

--- src/entity/NPC_Merchant.java
+++ src/entity/NPC_Merchant.java
@@ -31,9 +31,9 @@ public class NPC_Merchant extends Entity{
 
         up1 = setup("/npc/merchant_down_1", gp.tileSize, gp.tileSize);
         up2 = setup("/npc/merchant_down_2", gp.tileSize, gp.tileSize);
-        down1 = setup("/npc/merchant_down_2", gp.tileSize, gp.tileSize);
+        down1 = setup("/npc/merchant_down_1", gp.tileSize, gp.tileSize);
         down2 = setup("/npc/merchant_down_2", gp.tileSize, gp.tileSize);
-        left1 = setup("/npc/merchant_down_2", gp.tileSize, gp.tileSize);
+        left1 = setup("/npc/merchant_down_1", gp.tileSize, gp.tileSize);
         left2 = setup("/npc/merchant_down_2", gp.tileSize, gp.tileSize);
         right1 = setup("/npc/merchant_down_1", gp.tileSize, gp.tileSize);
         right2 = setup("/npc/merchant_down_2", gp.tileSize, gp.tileSize);
--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -1,5 +1,6 @@
 package main;
 
+import entity.NPC_Merchant;
 import entity.NPC_OldMan;
 import interactiveTiles.IT_DryTree;
 import monster.MON_GreenSlime;
@@ -46,10 +47,22 @@ public class AssetSetter {
     }
 
     public void setNPC(){
+
         int mapNum = 0;
-        gp.npc[mapNum][0] = new NPC_OldMan(gp);
-        gp.npc[mapNum][0].worldX = gp.tileSize * 21;
-        gp.npc[mapNum][0].worldY = gp.tileSize * 21;
+        int i = 0;
+        //MAP 0
+        gp.npc[mapNum][i] = new NPC_OldMan(gp);
+        gp.npc[mapNum][i].worldX = gp.tileSize * 21;
+        gp.npc[mapNum][i].worldY = gp.tileSize * 21;
+        i++;
+
+        mapNum = 1;
+        i = 0;
+        //MAP 1
+        gp.npc[mapNum][i] = new NPC_Merchant(gp);
+        gp.npc[mapNum][i].worldX = gp.tileSize * 12;
+        gp.npc[mapNum][i].worldY = gp.tileSize * 7;
+        i++;
     }
 
     public void setMonster(){
